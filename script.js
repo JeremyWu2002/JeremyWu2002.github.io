1d10f8c33fffcecb1d62dc26a2be7cc4e3902ee8
JeremyWu2002 <jeremy.wu73@gmail.com>
2021-07-20 14:15:03 -0700

    add button works for adding an item

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const dateElement = document.querySelector('.date');
 const list = document.getElementById('list');
 const input = document.getElementById('input');
 const finished = document.querySelectorAll('input[type=checkbox]');
+const add = document.querySelector('.btn');
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const line_through = 'lineThrough';
+
+const day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 let itemList = [];
 let id;
@@ -28,7 +36,41 @@ const options = { weekday: "long", month: "short", day: "numeric" };
 const date = new Date();
 
 dateElement.innerHTML = date.toLocaleDateString("en-US", options);
+const numericDate = date.toLocaleDateString(options);
+console.log(numericDate);
 
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+add.addEventListener('click', function (e) {
+    const toDo = activity.value;
+    const chosenDay = address.value;
+
+    newItem(toDo);
+    activity.value = '';
+
+    console.log(toDo, chosenDay);
+    closeModal();
+});
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
+
+// adding item to the list
 function addItem(item, number, done) {
     const line = done ? line_through : '';
     const required =
@@ -47,6 +89,7 @@ function addItem(item, number, done) {
 
 };
 
+// removing item from the list
 const removeItem = function (element) {
     /* be able to delete from itemList array to have localStorage work*/
     const position = element.parentNode.childNodes;
@@ -64,29 +107,48 @@ const removeItem = function (element) {
     element.parentNode.parentNode.removeChild(element.parentNode);
 };
 
+// adding line through to completed items
 const completeItem = function (element) {
     element.parentNode.classList.toggle(line_through);
-    console.log(element);
 
-    if (itemList[element.id].finished) {
-        itemList[element.id].finished = false;
-    }
-    else {
-        itemList[element.id].finished = true;
+    for (let u = 0; u < itemList.length; u++) {
+        if (element.id == itemList[u].each) {
+            if (itemList[u].finished) {
+                itemList[u].finished = false;
+            }
+            else {
+                itemList[u].finished = true;
+            }
+        }
     }
 };
 
+//clearing the list
 const clear = function () {
     localStorage.clear();
     location.reload();
 };
 
+//loading list from the local storage
 function loadList(array) {
     if (array) {
         array.forEach(element => addItem(element.name, element.each, element.finished));
     }
 };
 
+function newItem(adding) {
+    if (adding) {
+        addItem(adding, id, false)
+        itemList.push({
+            name: adding,
+            each: id,
+            finished: false,
+        })
+        localStorage.setItem("TODO", JSON.stringify(itemList));
+        id++;
+    }
+}
+//linking clicks on items to functions
 list.addEventListener('click', function (e) {
     const element = e.target;
     let job = '';
@@ -106,24 +168,19 @@ list.addEventListener('click', function (e) {
     localStorage.setItem("TODO", JSON.stringify(itemList));
 });
 
+//clearing when the reset button is clicked
 reset.addEventListener('click', function (e) {
     clear();
 });
 
+//adding list to the listem when the neter key is hit
 document.addEventListener("keyup", function (even) {
     if (event.keyCode === 13) {
         const toDo = input.value;
 
-        if (toDo) {
-            addItem(toDo, id, false)
-            itemList.push({
-                name: toDo,
-                each: id,
-                finished: false,
-            })
-            localStorage.setItem("TODO", JSON.stringify(itemList));
-            id++;
-        }
+        newItem(toDo);
         input.value = '';
     }
 });
+
+
