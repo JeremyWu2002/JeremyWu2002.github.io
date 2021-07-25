6c8aed93ffc246c6eeca479ebd9431cc2ad2836f
JeremyWu2002 <jeremy.wu73@gmail.com>
2021-07-25 12:12:02 -0700

    working version 3

--- script.js
+++ script.js
@@ -4,13 +4,10 @@ const list = document.getElementById('list');
 const input = document.getElementById('input');
 const finished = document.querySelectorAll('input[type=checkbox]');
 const add = document.querySelector('.btn');
-<<<<<<< HEAD
-=======
 const nav = document.querySelector('.nav');
 const container = document.querySelector('.container');
 const monday = document.querySelector('.monday');
 const header = document.querySelector('.header');
->>>>>>> parent of 781b727 (week update)
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
@@ -19,13 +16,31 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const line_through = 'lineThrough';
 
-const day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const day = ['Monday,', 'Tuesday,', 'Wednesday,', 'Thursday,', 'Friday,', 'Saturday,', 'Sunday,'];
+const thirtyOneDays = ['1', '3', '5', '7', '8', '10', '12'];
+const thirtyDays = ['4', '6', '9', '11'];
+const february = '2';
+let daysInMonth = 31;
+let currently = 0;
+let wantedDay = 0;
 
+/* finding today's date and separating it into arrays*/
+const options = { weekday: "long", month: "short", day: "numeric" };
+const date = new Date();
+
+dateElement.innerHTML = date.toLocaleDateString("en-US", options);
+const words = date.toLocaleDateString("en-US", options);
+const numericDate = date.toLocaleDateString();
+const separatedToday = words.split(' ');
+const separatedTodayNum = numericDate.split('/');
+
+console.log(numericDate, words, separatedToday, separatedTodayNum);
+
+//loading data for the current date
 let itemList = [];
 let id;
 
-let data = localStorage.getItem("TODO");
-
+let data = localStorage.getItem(numericDate);
 if (data) {
     itemList = JSON.parse(data);
     if (typeof itemList === "string") {
@@ -39,13 +54,17 @@ else {
     id = 0;
 }
 
-const options = { weekday: "long", month: "short", day: "numeric" };
-const date = new Date();
+/*find what day of the week it is*/
+function findTodayindex() {
+    for (let l = 0; l < day.length; l++) {
+        if (day[l] === separatedToday[0]) {
+            currently = l;
+        }
+    }
+}
+findTodayindex();
 
-dateElement.innerHTML = date.toLocaleDateString("en-US", options);
-const numericDate = date.toLocaleDateString(options);
-console.log(numericDate);
-
+/* openning and closing the modal*/
 const openModal = function () {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
@@ -63,49 +82,39 @@ overlay.addEventListener('click', closeModal);
 add.addEventListener('click', function (e) {
     const toDo = activity.value;
     const chosenDay = address.value;
-<<<<<<< HEAD
-
-    newItem(toDo);
-    activity.value = '';
-
-    console.log(toDo, chosenDay);
-    closeModal();
-});
-
-=======
     const day = chosenDay.split('-');
-
     if (day[0] == separatedTodayNum[2] && parseInt(day[1], 0) == parseInt(separatedTodayNum[0], 0) && day[2] == separatedTodayNum[1]) {
+        console.log('entered')
         newItem(toDo);
     }
-
-    const addingDay = `${parseInt(day[1], 0)}/${day[2]}/${day[0]}`;
-
-    let chosenDayList = [];
-    let num;
-
-    let info = localStorage.getItem(addingDay);
-    if (info) {
-        chosenDayList = JSON.parse(info);
-        if (typeof chosenDayList === "string") {
-            chosenDayList = [];
-        }
-        num = chosenDayList.length;
-        loadList(chosenDayList);
-    }
     else {
-        chosenDayList = [];
-        num = 0;
-    }
-    addingItemChosenDay(toDo, chosenDayList, addingDay, num);
-    console.log(chosenDayList);
-    activity.value = '';
+        const addingDay = `${parseInt(day[1], 0)}/${day[2]}/${day[0]}`;
 
-    console.log(toDo, chosenDay);
+        let chosenDayList = [];
+        let num;
+
+        let info = localStorage.getItem(addingDay);
+        if (info) {
+            chosenDayList = JSON.parse(info);
+            if (typeof chosenDayList === "string") {
+                chosenDayList = [];
+            }
+            num = chosenDayList.length;
+        }
+        else {
+            chosenDayList = [];
+            num = 0;
+        }
+        addingItemChosenDay(toDo, chosenDayList, addingDay, num);
+        console.log(chosenDayList);
+        activity.value = '';
+
+        console.log(toDo, chosenDay);
+    }
     closeModal();
 });
+
 // when modal is open, enter closes the modal
->>>>>>> parent of 781b727 (week update)
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
@@ -136,7 +145,7 @@ const removeItem = function (element) {
     /* be able to delete from itemList array to have localStorage work*/
     const position = element.parentNode.childNodes;
     const parent = position[1].removeChild(position[1].childNodes[0]);
-    localStorage.removeItem("TODO", parent);
+    localStorage.removeItem(numericDate, parent);
     for (let l = 0; l < itemList.length; l++) {
         if (element.id == itemList[l].each) {
             const temp = itemList.slice(l + 1, itemList.length)
@@ -178,6 +187,7 @@ function loadList(array) {
     }
 };
 
+/* adding new item to the local storage*/
 function newItem(adding) {
     if (adding) {
         addItem(adding, id, false)
@@ -186,7 +196,19 @@ function newItem(adding) {
             each: id,
             finished: false,
         })
-        localStorage.setItem("TODO", JSON.stringify(itemList));
+        localStorage.setItem(numericDate, JSON.stringify(itemList));
+        id++;
+    }
+}
+
+function addingItemChosenDay(adding, list, date, num) {
+    if (adding) {
+        list.push({
+            name: adding,
+            each: num,
+            finished: false,
+        })
+        localStorage.setItem(date, JSON.stringify(list));
         id++;
     }
 }
@@ -195,6 +217,7 @@ list.addEventListener('click', function (e) {
     const element = e.target;
     let job = '';
     let check = '';
+
     try {
         job = element.attributes.job.value;
     }
@@ -207,7 +230,7 @@ list.addEventListener('click', function (e) {
     if (check && check === 'INPUT') {
         completeItem(element);
     }
-    localStorage.setItem("TODO", JSON.stringify(itemList));
+    localStorage.setItem(numericDate, JSON.stringify(itemList));
 });
 
 //clearing when the reset button is clicked
@@ -225,9 +248,6 @@ document.addEventListener("keyup", function (even) {
     }
 });
 
-<<<<<<< HEAD
-
-=======
 // change color on nav when hovered
 const handleHover = function (e) {
     if (e.target.classList.contains('nav__link')) {
@@ -267,11 +287,81 @@ const stickyNav = function (entries) {
     }
 };
 
-
 const headerObserver = new IntersectionObserver(stickyNav, {
     root: null,
     threshold: 0,
     rootMargin: `-${navheight}px`,
 });
 headerObserver.observe(header);
->>>>>>> parent of 781b727 (week update)
+
+//showing the number of things to do in the week
+function printWeek() {
+    let add = 7 - currently;
+    let subtract = 0 + currently;
+
+    for (let l = 1; l < add; l++) {
+        const currentDate = `${separatedTodayNum[0]}/${parseInt(separatedTodayNum[1]) + l}/${separatedTodayNum[2]}`;
+        let chosenDayList = [];
+        let num;
+        let info = localStorage.getItem(currentDate);
+
+        const dayBoxId = 'day' + (currently + l)
+        const neededDay = document.getElementById(dayBoxId);
+
+        if (info) {
+            chosenDayList = JSON.parse(info);
+            if (typeof chosenDayList === "string") {
+                chosenDayList = [];
+            }
+            num = chosenDayList.length;
+        }
+        else {
+            chosenDayList = [];
+            num = 0;
+        }
+        if (chosenDayList.length === 0) {
+            neededDay.insertAdjacentHTML('afterbegin', '<h3 id="display">Nothing to do</h3>');
+        }
+        else {
+            neededDay.insertAdjacentHTML('afterbegin', `<h2 id="display">${chosenDayList.length} thing(s) to do</h2>`);
+        }
+
+    }
+    for (let l = 0; l <= subtract; l++) {
+        const currentDate = `${separatedTodayNum[0]}/${parseInt(separatedTodayNum[1]) - l}/${separatedTodayNum[2]}`;
+        let chosenDayList = [];
+        let num;
+
+        const dayBoxId = 'day' + (currently - l + 1)
+        const neededDay = document.getElementById(dayBoxId);
+
+        let info = localStorage.getItem(currentDate);
+        if (info) {
+            chosenDayList = JSON.parse(info);
+            if (typeof chosenDayList === "string") {
+                chosenDayList = [];
+            }
+            num = chosenDayList.length;
+        }
+        else {
+            chosenDayList = [];
+            num = 0;
+        }
+        if (chosenDayList.length === 0) {
+            neededDay.insertAdjacentHTML('afterbegin', '<h3 id="display">Nothing to do</h3>');
+        }
+        else {
+            neededDay.insertAdjacentHTML('afterbegin', `<h5 id="display">${chosenDayList.length} thing(s) to do</h5>`);
+        }
+    }
+}
+
+printWeek();
+function addNothing(daily, date) {
+
+
+}
+
+function addSomething() {
+
+}
